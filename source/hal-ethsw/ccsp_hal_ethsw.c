d310eae5cae67f1bf484a475b1ee363c529a4005
keerthana.p <keerthana.p@lnttechservices.com>
2019-06-24 10:49:26 +0000

    REFPLTB-344 : Implementation of CcspHalExtSw_getAssociatedDevice()
    - to get the wired clients information
    Reason for change: We need to implement this api to getting the
    connected wired clients information and updated the same in
    Connected_Device php via CCspLMLite
    Test Procedure: In webui,Gateway > Connected Devices > Devices
    Risks: Low
    
    Change-Id: I99c1705bce6b0a26e6f65ad54f28f5091df34da1
    Signed-off-by: keerthana.p <keerthana.p@lnttechservices.com>

--- source/hal-ethsw/ccsp_hal_ethsw.c
+++ source/hal-ethsw/ccsp_hal_ethsw.c
@@ -76,6 +76,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "ccsp_hal_ethsw.h" 
 
@@ -85,6 +86,9 @@
 **********************************************************************/
 
 #define  CcspHalEthSwTrace(msg)                     printf("%s - ", __FUNCTION__); printf msg;
+#define MAX_BUF_SIZE 1024
+#define MACADDRESS_SIZE 6
+#define LM_ARP_ENTRY_FORMAT  "%63s %63s %63s %63s %17s %63s"
 
 /**********************************************************************
                             MAIN ROUTINES
@@ -570,4 +574,292 @@ CcspHalEthSwLocatePortByMacAddress
     *pPortId = CCSP_HAL_ETHSW_EthPort4;
 
     return  RETURN_OK;
+}
+
+//For Getting Current Interface Name from corresponding hostapd configuration
+void GetInterfaceName(char *interface_name, char *conf_file)
+{
+        FILE *fp = NULL;
+        char path[MAX_BUF_SIZE] = {0},output_string[MAX_BUF_SIZE] = {0},fname[MAX_BUF_SIZE] = {0};
+        int count = 0;
+        char *interface = NULL;
+
+        fp = fopen(conf_file, "r");
+        if(fp == NULL)
+        {
+                printf("conf_file %s not exists \n", conf_file);
+                return;
+        }
+        fclose(fp);
+
+        sprintf(fname,"%s%s%s","cat ",conf_file," | grep interface=");
+        fp = popen(fname,"r");
+        if(fp == NULL)
+        {
+                        printf("Failed to run command in Function %s\n",__FUNCTION__);
+                        strcpy(interface_name, "");
+                        return;
+        }
+        if(fgets(path, sizeof(path)-1, fp) != NULL)
+        {
+                        interface = strchr(path,'=');
+
+                        if(interface != NULL)
+                                strcpy(output_string, interface+1);
+        }
+
+        for(count = 0;output_string[count]!='\n';count++)
+                        interface_name[count] = output_string[count];
+        interface_name[count]='\0';
+
+        fprintf(stderr,"Interface name %s \n", interface_name);
+
+        pclose(fp);
+}
+/* CcspHalExtSw_getAssociatedDevice :  */
+/**
+* @description Collected the active wired clients information
+
+* @param output_array_size    -- Size of the active wired connected clients
+* @param output_struct     -- Structure of  wired clients informations
+
+*
+* @return The status of the operation.
+* @retval RETURN_OK if successful.
+* @retval RETURN_ERR if any error is detected
+*
+*/
+
+INT CcspHalExtSw_getAssociatedDevice(ULONG *output_array_size, eth_device_t **output_struct)
+{
+	CHAR buf[MAX_BUF_SIZE] = {0},str[MAX_BUF_SIZE] = {0},interface_name[50] = {0},macAddr[50] = {0};
+	FILE *fp = NULL,*fp1 = NULL;
+	INT count = 0,str_count = 0;
+	ULONG maccount = 0,eth_count = 0;
+	INT arr[MACADDRESS_SIZE] = {0};
+	UCHAR mac[MACADDRESS_SIZE] = {0};
+	CHAR ipAddr[50],stub[50],phyAddr[50],ifName[32],status[32];
+	int ret;
+	if(output_struct == NULL)
+	{
+		printf("\nNot enough memory\n");
+		return RETURN_ERR;
+	}
+	if( access( "/tmp/ethernetmac.txt", F_OK ) != -1 ) {
+		remove("/tmp/ethernetmac.txt");
+	}
+	system("cat /nvram/dnsmasq.leases | cut -d ' ' -f2 > /tmp/connected_mac.txt"); //storing the all associated device information in tmp folder
+	//storing the private wifi  associated device iformation in tmp folder
+	GetInterfaceName(interface_name,"/nvram/hostapd0.conf");
+	sprintf(buf,"iw dev %s station dump | grep Station | cut -d ' ' -f2 > /tmp/Associated_Devices.txt",interface_name);
+	system(buf);
+	GetInterfaceName(interface_name,"/nvram/hostapd1.conf");
+	sprintf(buf,"iw dev %s station dump | grep Station | cut -d ' ' -f2 >> /tmp/Associated_Devices.txt",interface_name);
+	system(buf);
+
+	system("diff /tmp/Associated_Devices.txt /tmp/connected_mac.txt | grep \"^+\" | cut -c2- | sed -n '1!p' > /tmp/ethernet_connected_clients.txt"); //separating the ethernet associated device information from connected_mac test file
+	fp=popen("cat /tmp/ethernet_connected_clients.txt | wc -l","r"); // For getting the  ethernet connected mac count
+	if(fp == NULL)
+		return RETURN_ERR;
+	else
+	{
+		fgets(buf,MAX_BUF_SIZE,fp);
+		maccount = atol(buf);
+		fprintf(stderr,"ethernet umac is %d \n",maccount);
+	}
+	pclose(fp);
+	eth_device_t *temp=NULL;
+	temp = (eth_device_t*)calloc(1, sizeof(eth_device_t)*maccount);
+	if(temp == NULL)
+	{
+		fprintf(stderr,"Not enough memory \n");
+		return RETURN_ERR;
+	}
+	fp=fopen("/tmp/ethernet_connected_clients.txt","r"); // reading the ethernet associated device information
+	if(fp == NULL)
+	{
+		*output_struct = NULL;
+		*output_array_size = 0;
+		return RETURN_ERR;
+	}
+	else
+	{
+		for(count = 0;count < maccount ; count++)
+		{
+			fgets(str,MAX_BUF_SIZE,fp);	
+			for(str_count = 0;str[str_count]!='\n';str_count++)
+				macAddr[str_count] = str[str_count];
+			macAddr[str_count] = '\0';
+			system("ip nei show | grep brlan0 > /tmp/arp_cache");
+			fp1=fopen("/tmp/arp_cache","r");
+			if(fp1 == NULL)
+				return RETURN_ERR;
+
+			while(fgets(buf,sizeof(buf),fp1) != NULL)
+			{
+				if ( strstr(buf, "FAILED") != 0 )
+					continue;
+				/*
+Sample:
+10.0.0.208 dev brlan0 lladdr d4:be:d9:99:7f:47 STALE
+10.0.0.107 dev brlan0 lladdr 64:a2:f9:d2:f5:67 REACHABLE
+				 */
+				ret = sscanf(buf, LM_ARP_ENTRY_FORMAT,
+						ipAddr,
+						stub,
+						ifName,
+						stub,
+						phyAddr,
+						status);  
+				if(ret != 6)
+					continue;
+				if(strcmp(phyAddr,macAddr) == 0)
+				{
+					memset(buf,0,sizeof(buf));
+					if(strcmp(status,"REACHABLE") == 0)
+					{
+						sprintf(buf,"echo %s >> /tmp/ethernetmac.txt",macAddr);
+						system(buf);
+						eth_count++;
+						break;
+					}
+					else if((strcmp(status,"STALE") == 0) || (strcmp(status,"DELAY")))
+					{
+						sprintf(buf,"ping -q -c 1 -W 1  \"%s\"  > /dev/null 2>&1",ipAddr);
+						fprintf(stderr,"buf is %s and MACADRRESS %s\n",buf,macAddr);
+						if (WEXITSTATUS(system(buf)) == 0)
+						{
+							fprintf(stderr,"Inside STALE SUCCESS \n");
+							memset(buf,0,sizeof(buf));
+							sprintf(buf,"echo %s >> /tmp/ethernetmac.txt",macAddr);
+							system(buf);
+							eth_count++;
+							break;
+						}
+					}
+					else
+					{
+						fprintf(stderr,"Running in different state \n");
+						break;
+					}
+				}
+				else
+					fprintf(stderr,"MAcAddress is not valid \n");
+			}
+			fclose(fp1);
+		}
+	}
+	fclose(fp);
+	fp=fopen("/tmp/ethernetmac.txt","r");
+	if(fp == NULL)
+	{
+		*output_struct = NULL;
+		*output_array_size = 0;
+		return RETURN_OK;
+	}
+	else
+	{
+		memset(buf,0,sizeof(buf));
+		for(count = 0;count < eth_count ; count++)
+		{
+			fgets(buf,sizeof(buf),fp);
+				if(MACADDRESS_SIZE  == sscanf(buf, "%02x:%02x:%02x:%02x:%02x:%02x",&arr[0],&arr[1],&arr[2],&arr[3],&arr[4],&arr[5]) )
+				{
+					for( int ethclientindex = 0; ethclientindex < 6; ++ethclientindex )
+					{
+						mac[ethclientindex] = (unsigned char) arr[ethclientindex];
+					}
+					memcpy(temp[count].eth_devMacAddress,mac,(sizeof(unsigned char))*6);
+					fprintf(stderr,"MAC %d = %X:%X:%X:%X:%X:%X \n", count, temp[count].eth_devMacAddress[0],temp[count].eth_devMacAddress[1], temp[count].eth_devMacAddress[2], temp[count].eth_devMacAddress[3], temp[count].eth_devMacAddress[4], temp[count].eth_devMacAddress[5]);
+				}
+			temp[count].eth_port=1;
+			temp[count].eth_vlanid=10;
+			temp[count].eth_devTxRate=100;
+			temp[count].eth_devRxRate=100;
+			temp[count].eth_Active=1;
+
+		}
+	}
+	fclose(fp);
+	*output_struct = temp;
+	*output_array_size = eth_count;
+	fprintf(stderr,"Connected Active ethernet clients count is %ld \n",*output_array_size);
+	return 	RETURN_OK;
+}
+
+/* CcspHalExtSw_getEthWanEnable  */
+/**
+* @description Return the Ethwan Enbale status
+
+* @param enable    -- Having status of WANMode ( Ethernet,DOCSIS)
+
+*
+* @return The status of the operation.
+* @retval RETURN_OK if successful.
+* @retval RETURN_ERR if any error is detected
+*
+*/
+
+INT CcspHalExtSw_getEthWanEnable(BOOLEAN *enable)
+{
+	*enable = 1; // Raspberrypi doesn't have docsis support.so, it always return as 1.
+	return RETURN_OK;
+}
+
+/* CcspHalExtSw_getEthWanPort:  */
+/**
+* @description Return the ethwan port
+
+* @param port    -- having ethwan port
+
+*
+* @return The status of the operation.
+* @retval RETURN_OK if successful.
+* @retval RETURN_ERR if any error is detected
+*
+*/
+
+INT CcspHalExtSw_getEthWanPort(UINT *Port)
+{
+	*Port = 20;
+	return RETURN_OK;
+}
+
+/* CcspHalExtSw_setEthWanEnable :  */
+/**
+* @description setting the ethwan enable status
+
+* @enable    -- Switch from ethernet mode to docsis mode or vice-versa
+
+*
+* @return The status of the operation.
+* @retval RETURN_OK if successful.
+* @retval RETURN_ERR if any error is detected
+*
+*/
+
+INT CcspHalExtSw_setEthWanEnable(BOOLEAN enable)
+{
+	enable = 0;
+	return RETURN_OK;
+}
+
+
+/* CcspHalExtSw_setEthWanPort :  */
+/**
+* @description  Need to set the ethwan port
+
+* @param port    -- Setting the ethwan port
+
+*
+* @return The status of the operation.
+* @retval RETURN_OK if successful.
+* @retval RETURN_ERR if any error is detected
+*
+*/
+
+INT CcspHalExtSw_setEthWanPort(UINT Port)
+{
+	Port = 20;
+	return RETURN_OK;
 }
